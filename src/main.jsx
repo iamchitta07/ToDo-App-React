debda439f69f3937f59c18cb859fd06c8485dfb5
iamchitta07 <nathchittajit8@gmail.com>
2025-06-05 14:26:20 +0530

    CSS added in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import Layout from "./Layout.jsx";
 import { StrictMode } from 'react';
+import "./styles.css"
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
