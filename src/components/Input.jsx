0d98ef25ebaa45b4a7583d96d79ca05e89c4e4e8
iamchitta07 <nathchittajit8@gmail.com>
2025-06-05 19:47:53 +0530

    Removed PostCSS

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -34,7 +34,7 @@ function Input({fill}) {
                 </div>
                 <div className="lg:w-4/10">
                     <label className="text-white font-bold mb-3 select-none md:block text-xl md:text-2xl lg:text-3xl"> Description: </label>
-                    <input type="text" ref={desRef} className="ocus:outline-2 focus:outline-[#00B84F] focus:drop-shadow-[#00a51A]/30 focus:drop-shadow-xl/25 bg-white rounded-md w-full text-md md:text-xl py-2 px-3 xl:py-3 xl:px-4 xl:text-2xl" placeholder="Description of your To-Do ?"/>
+                    <input type="text" ref={desRef} className="focus:outline-2 focus:outline-[#00B84F] focus:drop-shadow-[#00a51A]/30 focus:drop-shadow-xl/25 bg-white rounded-md w-full text-md md:text-xl py-2 px-3 xl:py-3 xl:px-4 xl:text-2xl" placeholder="Description of your To-Do ?"/>
                 </div>
                 <div className="pt-8">
                     <button type='submit' className="font-semibold bg-[#00B84F] hover:bg-[#00a51A] hover:drop-shadow-md hover:drop-shadow-[#00a51A]/20  rounded-xl text-white text-md md:text-xl w-full py-2 lg:px-8 xl:px-10 xl:py-3 xl:text-2xl">Add</button>
